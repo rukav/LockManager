c1cad6cfda82b2dad49e4bd6dc4f21670fdec8e6
Vitaliy Rukavishnikov <vitaliyrukavishnikov@Macintosh.local>
2011-05-08 16:09:18 -0400

    Added new methods to the hash table

--- src/Control/Concurrent/THashTable.hs
+++ src/Control/Concurrent/THashTable.hs
@@ -1,4 +1,5 @@
 module Control.Concurrent.THashTable (
+   THashTable,
    newIO,
    new,
    hashString,
@@ -14,34 +15,65 @@ import Control.Concurrent.STM
 import Data.Digest.Murmur32
 import Data.Digest.Murmur64
 
+type Hash = Int
+type Bucket k v = HM.HashMap Hash [(k,v)]
+
 data THashTable k v = THashTable {
-   buckets :: V.Vector (TVar (Bucket k v)),
-   hashFun :: k -> Word
+   table :: V.Vector (TVar (Bucket k v)),
+   hashFun :: k -> Hash
 }
 
-type Bucket k v = HM.HashMap Word [(k,v)]
-
-newIO :: Eq k => Int -> (k -> Word) -> IO (THashTable k v)
+newIO :: Eq k => Int -> (k -> Hash) -> IO (THashTable k v)
 newIO capacity hf = atomically $ new capacity hf
 
-new :: Eq k => Int -> (k -> Word) -> STM (THashTable k v)
+new :: Eq k => Int -> (k -> Hash) -> STM (THashTable k v)
 new capacity hf = do
-   let len = powerOfTwoSize capacity
+   let len = powerOf2 capacity
    bs <- V.replicateM len (newTVar HM.empty)
    return $ THashTable bs hf
+   where
+     powerOf2 n = last $ takeWhile (< n) (iterate (flip shiftL 1) 1) 
 
-bucketIndex :: Word -> THashTable k v -> Word
-bucketIndex h t = h .&. toEnum (n - 1)
-   where n = V.length $ buckets t 
+lookup :: Eq k => k -> THashTable k v -> STM (Maybe v)
+lookup key tbl = do
+   hm <- readTVar tvar
+   return $! maybe Nothing (Prelude.lookup key) (look hm) 
+   where
+     hash = (hashFun tbl) key
+     tvar = bucketFor hash tbl
+     look = HM.lookup hash
 
-powerOfTwoSize :: Int  -> Int
-powerOfTwoSize n = last $ takeWhile (< n) (iterate (flip shiftL 1) 1) 
+insert :: Eq k => k -> v -> THashTable k v -> STM ()
+insert key val tbl = do
+   hm <- readTVar tvar
+   writeTVar tvar (ins hm)
+   where
+     hash = (hashFun tbl) key
+     tvar = bucketFor hash tbl
+     ins = HM.insertWith (++) hash [(key,val)]
 
-hashString :: String -> Word
+delete :: Eq k => k -> THashTable k v -> STM ()
+delete key tbl = do
+   hm <- readTVar tvar
+   writeTVar tvar (del hm)
+   where
+     hash = (hashFun tbl) key
+     tvar = bucketFor hash tbl
+     vals hm = maybe [] clean (HM.lookup hash hm)
+     clean = filter (\(k,_) -> k /= key)
+     del hm = HM.insert hash (vals hm) hm
+
+bucketFor :: Hash -> THashTable k v -> TVar (Bucket k v)
+bucketFor hash tbl = V.unsafeIndex buckets ind
+   where
+     ind = hash .&. (V.length buckets) - 1
+     buckets = table tbl
+
+hashString :: String -> Int
 hashString s = if has32bits then fromIntegral $ asWord32 $ hash32 s 
                 else fromIntegral $ asWord64 $ hash64 s
 
-hashInt :: Int -> Word
+hashInt :: Int -> Int
 hashInt d = if has32bits then fromIntegral $ asWord32 $ hash32 d 
              else fromIntegral $ asWord64 $ hash64 d
 
